28d8f84cce2f503f037f2192c10a7b3a8fb0a770
Ameerahalshihry <Ameerahalshihry@gmail.com>
2019-04-09 20:35:46 +0300

    solve

---
+++ Employee.rb
@@ -0,0 +1,62 @@
+class Employee 
+    # attr_accessor :name
+    # attr_accessor :level
+    # attr_accessor :money_balance
+    # attr_reader  :absence_account , :single_day_wage 
+    def initialize (name, level)
+        @name=name
+        @level=level
+        @money_balance = 0
+        @lates_count = 0
+        @absence_account = 20
+        @single_day_wage = 0
+    end
+    def absent
+        @absence_account = @absence_account - 1
+        # @absent_day =  @absent_day  + 1
+    end
+    def late
+        @lates_count =  @lates_count + 1
+        if @lates_count == 3
+            @absence_account = @absence_account - 1
+            # @vac_days = @vac_days - 1
+            end
+    end
+    def add_balance
+        if @level == 1
+            @money_balance = 2000
+        elsif @level == 2
+            @money_balance = 4000
+        elsif @level == 3
+            @money_balance = 6000
+        end
+        @single_day_wage = @money_balance / 30
+        @money_balance = @money_balance - (@single_day_wage * (20 - @absence_account))
+        # money_balance = salary - (single_day_wage * @absent_day)
+    end
+    def check
+        # check method that shows the employee's name, level, number of days left in their absence account, number of times they were late, and their balance
+    p "The employee name is : #{@name}"
+    p "The level is : #{@level}"
+    p "The number of days left in his absence account : #{@absence_account} days"
+    p "The number of times he was late : #{@lates_count} times"
+    p "The money balance is : #{@money_balance} SR"
+    end
+end
+
+ameerah = Employee.new("Ameerah",3)
+ameerah.absent
+ameerah.add_balance
+ameerah.check
+
+saad = Employee.new("Saad",2)
+saad.late
+saad.add_balance
+saad.check
+
+sama=Employee.new("Sama", 1)
+sama.late
+sama.late
+sama.late
+sama.add_balance
+sama.check
