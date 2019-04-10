0394e02d08e8e90822dbfe8a9e1cf3dcc7a9b604
Reema Al-Rayes <reema@Reemas-MacBook-Pro.local>
2019-04-10 09:10:01 +0300

    w7_d3

---
+++ sample.rb
@@ -0,0 +1,37 @@
+# class employee
+#     def initialize
+#         @employees = [
+#           {name: "Reema", level: "Level 1"},
+#           {name: "Mohammed", level: "Level 3"},
+#           {name: "Sarah", level: "Level 2"},
+#           {name: "Norah", level: "Level 2"},
+#           {name: "Ahmed", level: "Level 1"}] 
+#       end
+
+#         def late
+#         late = 0
+#         end
+
+#       def absents
+#         absent = 20
+#         if absent==0
+#             puts "you exceeded your limit of absents"
+#         else
+#             late = late-3
+#             absent= absent-1
+#         end
+#     end
+
+
+
+#       def balance 
+#         balance = 0 
+#       end
+
+
+#       def add_balance = 
+#         #to add to the emoloyee balance 
+#       end
+    
+#        p  "#{employee[:name]}  ==> #{employee[:level]}"
+#        p "Absents" + #{absents} + "Lates" + #{late} + "Balance" + #{balance}
